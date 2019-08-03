6f60ce010d656721ec758d260fbb48677fc58fc8
jatin77 <clockstopfountain@gmail.com>
2019-08-03 19:12:45 +0530

    sec commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Go react
+          continous integration
         </a>
       </header>
     </div>

9e81a55e5a6d14e57801577ec9fa8fdaeed8bad5
jatin77 <clockstopfountain@gmail.com>
2020-02-22 14:04:23 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,41 +1,60 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { LinkedInPopUp } from "react-linkedin-login-oauth2";
+// import { LinkedInPopUp } from "react-linkedin-login-oauth2";
 
-import { LinkedIn } from "react-linkedin-login-oauth2";
+// import { LinkedIn } from "react-linkedin-login-oauth2";
+import LinkedIn from "linkedin-login-for-react";
 
 class LinkedInPage extends Component {
   state = {
-    code: "",
-    errorMessage: ""
+    // code: "",
+    // errorMessage: ""
   };
 
-  handleSuccess = data => {
-    console.log(data);
-    this.setState({
-      code: data.code,
-      errorMessage: ""
-    });
-  };
+  // handleSuccess = data => {
+  //   console.log(data);
+  //   this.setState({
+  //     code: data.code,
+  //     errorMessage: ""
+  //   });
+  // };
 
-  handleFailure = error => {
-    console.log(error);
+  // handleFailure = error => {
+  //   console.log(error);
 
-    this.setState({
-      code: "",
-      errorMessage: error.errorMessage
-    });
+  //   this.setState({
+  //     code: "",
+  //     errorMessage: error.errorMessage
+  //   });
+  // };
+  callbackLinkedIn = (
+    error,
+    code,
+    redirectUri = "https://dazzling-jang-a707c9.netlify.com"
+  ) => {
+    if (error) {
+      // signin failed
+    } else {
+      // Obtain authorization token from linkedin api
+      // see https://developer.linkedin.com/docs/oauth2 for more info
+    }
   };
 
   render() {
     return (
       <div>
-        <BrowserRouter>
+        {/* <BrowserRouter>
           <Switch>
             <Route exact path="/linkedin" component={LinkedInPopUp} />
           </Switch>
-        </BrowserRouter>
+        </BrowserRouter> */}
 
+        <LinkedIn
+          clientId="817ew6hqc3jaeg"
+          callback={this.callbackLinkedIn}
+          text="Login With LinkedIn"
+        />
+        {/* 
         <LinkedIn
           clientId="817ew6hqc3jaeg"
           scope="r_liteprofile r_emailaddress w_member_social"
@@ -47,7 +66,7 @@ class LinkedInPage extends Component {
               Custom linkedin element
             </button>
           )}
-        />
+        /> */}
       </div>
     );
   }

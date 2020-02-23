7963794cf4f29c4f32e3a47881aba118a44ccd56
jatin77 <clockstopfountain@gmail.com>
2020-02-23 23:23:30 +0530

    asd

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { LinkedInPopUp } from "./social/linked";
 // import Auth from "./try2/Auth.jsx";
+import CustomLinkedIN from "./CustomLinkedIN";
 
 class LinkedInPage extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <CustomLinkedIN />
       </div>
     );
   }
